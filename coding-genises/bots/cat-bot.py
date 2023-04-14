262150c1d70a03f4975b1f01ac7557d5ced4922f
caraz22 <cszoke22@gmail.com>
2023-04-14 17:55:31 -0400

    frustrated

--- coding-genises/bots/cat-bot.py
+++ coding-genises/bots/cat-bot.py
@@ -5,23 +5,32 @@ from discord.ext import commands
 
 load_dotenv()
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHANNEL_ID = os.environ.get("CHANNEL_ID")
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = discord.bot(intents=intents)
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+    global total_messages
+    total_messages = 0
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
+    
+    total_messages += 1
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('!count'):
+        await message.channel.send(total_messages)
+
+@bot.command()
+async def count(ctx):
+    if ctx.content.includes('count'):
+        await ctx.channel.send(total_messages)
 
 bot.run(BOT_TOKEN)
